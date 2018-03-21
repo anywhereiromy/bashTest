592bc42e541ee5fe7484625d258f827a40903076
Romy Greenfield <romygreenfield@Romys-MacBook-Pro.local>
2018-03-21 18:03:09 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+    // DO NOT CHANGE THE NAME OF THIS FUNCTION
+    function marta() {
+        // insert your code here
+    }
+    function romy() {
+        // insert your code here
+    } 
+    
+    module.exports = { marta, romy }
+    
+    
---
+++ spec/index.spec.js
@@ -0,0 +1,9 @@
+
+    const {expect} = require("chai")
+    const { marta, romy} = require("../index.js")
+    
+    describe("", () => {
+        it("", () => {
+            expect(romy()).to.equal(marta());
+        })
+    })
